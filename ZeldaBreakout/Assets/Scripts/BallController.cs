3e725fed4f76034efb3b04f400e35c1d618e31cd
Nicholas Staracek <nic.staracek@gmail.com>
2015-08-18 11:27:36 +1000

    MDU112.3 Assessment 'ZeldaBreakout'
    Intial Upload

---
+++ ZeldaBreakout/Assets/Scripts/BallController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBall : MonoBehaviour {
+	
+	public float ballVelocity = 150f;
+
+	public Rigidbody2D rb;
+	public bool ballInPlay;
+	public float ballSpeed = 1.5f;
+	
+	void Awake () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(ballVelocity, ballVelocity, 0));
+		}
+	}
+}
---
+++ ZeldaBreakout/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPaddleController : MonoBehaviour {
+	
+	public float playerPaddleSpeed = 1f;
+
+	private Vector3 playerPaddlePosition = new Vector3 (0, -4.6f, 0);
+	
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * playerPaddleSpeed);
+		playerPaddlePosition = new Vector3(Mathf.Clamp (xPos, -4.3f, 4.3f), -4.3f, 0f);
+		transform.position = playerPaddlePosition;
+	}
+}
